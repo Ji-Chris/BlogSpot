37b460efec82fe26ac599e746282696f18ce3f3b
Ji-Chris <kisnaanand623@gmail.com>
2026-01-15 16:54:58 +0530

    Added individual blog component

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
-import "./globals.css"
 import Image from "next/image"
 
 export default function HomePage(){
     return(
         <div id="main-container" className="flex flex-row justify-start ">
-            <section id="blog-list" className="grid-cols-1  w-full p-6">
+            <section id="blog-list" className="flex flex-col  w-full p-6">
                 <div className="flex justify-end gap-3 border border-black/50 p-3">
 
                     <button id="sort-button" className="border-2 p-2 rounded hover:bg-gray-100">
-                        <Image src="/sort-icon.svg" alt="sort" width={12} height={12}
-                        ></Image>
+                        <Image src="/sort-icon.svg" alt="sort" width={12} height={12} />
                     </button>
                     <button id="filter-button" className="border-2 p-2 rounded hover:bg-gray-100">
-                        <Image src="/filter-32.svg" alt="sort" width={12} height={12}
-                        ></Image>
+                        <Image src="/filter-32.svg" alt="sort" width={12} height={12} />
                     </button>
 
                 </div>
