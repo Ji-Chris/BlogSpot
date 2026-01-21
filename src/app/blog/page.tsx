61fb676b64730e85efeedc7c2a16cc22dbdd4019
Ji-Chris <kisnaanand623@gmail.com>
2026-01-21 15:58:37 +0530

    redsigned structure of the app

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -31,44 +31,57 @@ export default function BlogPage(){
     ];
 
     return(
-        <div className="blog-container ">
-            <h1>Blog List</h1>
+        <div id="main-container" className="flex flex-row justify-start ">
+            <section id="blog-list" className="flex flex-col  w-full p-6">
+                <div className="flex justify-end gap-3 border border-black/50 p-3 m-5">
 
-            <div className="">
-                {posts.map((post)=>{
-                    return(
-                        <div className="individual-blog grid grid-cols-4 justify-start">
-                            <div>
-                                <Link href={`/blog/${post.id}`}>
-                                    <Image 
-                                    src={post.imageUrl}
-                                    alt="blog-image"
-                                    width={100}
-                                    height={100}
-                                    />
-                                </Link>
-                                
+                    <button id="sort-button" className="border-2 p-2 rounded hover:bg-gray-100">
+                        <Image src="/sort-icon.svg" alt="sort" width={12} height={12} />
+                    </button>
+                    <button id="filter-button" className="border-2 p-2 rounded hover:bg-gray-100">
+                        <Image src="/filter-32.svg" alt="sort" width={12} height={12} />
+                    </button>
+
+                </div>
+
+                <div className="blog-list m-5">
+                    {posts.map((post)=>{
+                        return(
+                            <div key={post.id} className="individual-blog grid grid-cols-4 justify-start">
                                 <div>
-                                    <Image 
-                                    src={post.pfpUrl}
-                                    alt="author-image"
-                                    width={10}
-                                    height={10}
-                                    />
-                                    <h4>{post.authorName}</h4>
+                                    <Link href={`/blog/${post.id}`}>
+                                        <Image 
+                                        src={post.imageUrl}
+                                        alt="blog-image"
+                                        width={100}
+                                        height={100}
+                                        />
+                                    </Link>
+
+                                    <div>
+                                        <Image 
+                                        src={post.pfpUrl}
+                                        alt="author-image"
+                                        width={10}
+                                        height={10}
+                                        />
+                                        <h4>{post.authorName}</h4>
+                                    </div>
                                 </div>
+                                <div className="flex flex-col">
+                                    <h2>{post.title}</h2>
+                                    <p>{post.body}</p>
+                                </div>
+
                             </div>
-                            <div className="flex flex-col">
-                                <h2>{post.title}</h2>
-                                <p>{post.body}</p>
-                            </div>
-                            
-                        </div>
-                        
-                    );
-                })}
-            </div>
-        </div>
+
+                        );
+                    })}
+                </div>
+            
+            </section>
+
+        </div>      
         
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,6 @@ import Image from "next/image"
 
 export default function HomePage(){
     return(
-        <div id="main-container" className="flex flex-row justify-start ">
-            <section id="blog-list" className="flex flex-col  w-full p-6">
-                <div className="flex justify-end gap-3 border border-black/50 p-3">
-
-                    <button id="sort-button" className="border-2 p-2 rounded hover:bg-gray-100">
-                        <Image src="/sort-icon.svg" alt="sort" width={12} height={12} />
-                    </button>
-                    <button id="filter-button" className="border-2 p-2 rounded hover:bg-gray-100">
-                        <Image src="/filter-32.svg" alt="sort" width={12} height={12} />
-                    </button>
-
-                </div>
-
-                <div>
-                    <p>Blog List</p>
-                </div>
-            </section>
-
-        </div>
+        <h1>Home Page</h1>
     )
 }
