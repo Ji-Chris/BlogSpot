96c19fa4aa85def507ec54b785fddae1a55a998f
Ji-Chris <kisnaanand623@gmail.com>
2026-02-17 14:48:02 +0530

    adding simple email signIn

--- apps/web/src/app/api/auth/[...all]/route.ts
+++ apps/web/src/app/api/auth/[...all]/route.ts
@@ -22,5 +22,26 @@ export { handler as GET, handler as POST }
 
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
+import { authClient } from "@/lib/auth-client"
 
 export const { POST, GET } = toNextJsHandler(auth)
+
+
+const { data, error } = await authClient.signUp.email({
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        image, // User image URL (optional)
+        callbackURL: "/"
+    }, {
+        onRequest: (ctx) => {
+            //show loading
+        },
+        onSuccess: (ctx) => {
+            //redirect to the dashboard or sign in page
+        },
+        onError: (ctx) => {
+            // display the error message
+            alert(ctx.error.message);
+        },
+});
