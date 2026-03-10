811bc749ced4ae9196e4a61f7d0a05feda80a9d3
Ji-Chris <kisnaanand623@gmail.com>
2026-03-10 16:39:36 +0530

    oauth sessions done

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -1,11 +1,15 @@
-
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/lib/db";
+import * as schema from "@/lib/schema";
 
 export const auth = betterAuth({
 
-  emailAndPassword: {
-    enabled:true
-  },
+  database: drizzleAdapter(db,{
+    provider: "pg" ,
+    schema,
+  }),
+  emailAndPassword: { enabled:true },
   
   socialProviders: { 
     github: { 
